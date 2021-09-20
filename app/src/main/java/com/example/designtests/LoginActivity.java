1d2e033455f56fcd78209ed202f1d277ecd65feb
swumplurd <53883345+Swumplurd@users.noreply.github.com>
2021-09-20 11:14:55 -0500

    se agrego firebase
    se corrigio el tipo de los edit text

--- app/src/main/java/com/example/designtests/LoginActivity.java
+++ app/src/main/java/com/example/designtests/LoginActivity.java
@@ -1,37 +1,58 @@
 package com.example.designtests;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
 
-    private EditText email, password;
-    private Button iniciar_secion;
+    private FirebaseAuth mAuth;
+    private EditText correo;
+    private EditText password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        email = (EditText) findViewById(R.id.et_email_login);
-        password = (EditText) findViewById(R.id.et_password_login);
-        iniciar_secion = (Button) findViewById(R.id.btn_is_login);
-
-        iniciar_secion.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                logear(view);
-            }
-        });
+        mAuth = FirebaseAuth.getInstance();
+        correo = findViewById(R.id.et_email_login);
+        password = findViewById(R.id.et_password_login);
 
     }
-    private void logear (View view){
-        Intent logear = new Intent(this, DivisorActivity.class);
-        startActivity(logear);
+
+    public void onStart() {
+        super.onStart();
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+    }
+
+    public void iniciarSesion(View view){
+        mAuth.signInWithEmailAndPassword(correo.getText().toString(), password.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>(){
+
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(getApplicationContext(), "Bienvenido.", Toast.LENGTH_SHORT).show();
+                            Intent i = new Intent(getApplicationContext(), DivisorActivity.class);
+                            startActivity(i);
+                        } else {
+                            Toast.makeText(getApplicationContext(), "La autenticacion fallo!.", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
