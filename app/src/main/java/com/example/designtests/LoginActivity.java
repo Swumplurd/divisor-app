df89f665b17583d572e3cdc0b206e016a0c9d0f5
Posi-10 <64515605+Posi-10@users.noreply.github.com>
2021-09-20 09:24:30 -0500

    Inplementación de input, button y splashsrceen
    Solicitud de revicion y implementacion a la original

--- app/src/main/java/com/example/designtests/LoginActivity.java
+++ app/src/main/java/com/example/designtests/LoginActivity.java
@@ -2,13 +2,36 @@ package com.example.designtests;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText email, password;
+    private Button iniciar_secion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        email = (EditText) findViewById(R.id.et_email_login);
+        password = (EditText) findViewById(R.id.et_password_login);
+        iniciar_secion = (Button) findViewById(R.id.btn_is_login);
+
+        iniciar_secion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                logear(view);
+            }
+        });
+
+    }
+    private void logear (View view){
+        Intent logear = new Intent(this, DivisorActivity.class);
+        startActivity(logear);
     }
 }
